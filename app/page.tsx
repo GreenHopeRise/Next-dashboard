6464ca4a49e6acc35761c451dce06d96ef6af7e6
GreenHopeRise <khalidanwarantur96@gmail.com>
2026-07-18 17:00:03 +0600

    start

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
-
-export default function page() {
+export default function HomePage() {
   return (
-    <div>page</div>
-  )
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        SaaS Admin Dashboard
+      </h1>
+    </main>
+  );
 }
