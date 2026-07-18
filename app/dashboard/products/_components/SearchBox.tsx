7e3d6739dcb0e8dc7e6d6453e77cfd3f385683c0
GreenHopeRise <khalidanwarantur96@gmail.com>
2026-07-18 22:03:27 +0600

    Complete Module 28 - URL search params and search state

--- app/dashboard/products/_components/SearchBox.tsx
+++ app/dashboard/products/_components/SearchBox.tsx
@@ -1,31 +1,31 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function SearchBox() {
   const router = useRouter();
   const searchParams = useSearchParams();
-
-  function handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
-    const value = e.target.value;
-
+const handleSearch= useDebouncedCallback((value:string)=>{
     const params = new URLSearchParams(searchParams);
 
-    if (value.trim()) {
-      params.set("search", value);
-    } else {
-      params.delete("search");
-    }
-
-    router.push(`/dashboard/products?${params.toString()}`);
+  if (value.trim()) {
+    params.set("search", value);
+    params.set('page','1')
+  } else {
+    params.delete("search");
   }
+
+  router.push(`/dashboard/products?${params.toString()}`);
+},3000)
+
 
   return (
     <input
       type="text"
       placeholder="Search products..."
       defaultValue={searchParams.get("search") ?? ""}
-      onChange={handleSearch}
+      onChange={(e)=>handleSearch(e.target.value)}
       className="border rounded-md px-3 py-2"
     />
   );
