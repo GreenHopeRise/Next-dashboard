baa0122fee98036c798e527be59796e82b6990bd
GreenHopeRise <khalidanwarantur96@gmail.com>
2026-07-20 16:31:59 +0600

    done

--- app/products-demo/_components/DataTable.tsx
+++ app/products-demo/_components/DataTable.tsx
@@ -2,33 +2,31 @@
 import Image from "next/image";
 import { Product } from "../types/products";
 import { productColumns } from "../_data/columns";
+// import { Button } from "@base-ui/react";
 // import { Product } from "../_types/product";
+import { Button } from "@/components/ui/button";
+import { Eye, Pencil, Trash2 } from "lucide-react";
 
 type DataTableProps = {
   products: Product[];
 };
 
-export default function DataTable({
-  products,
-}: DataTableProps) {
+export default function DataTable({ products }: DataTableProps) {
   return (
     <table className="w-full border-collapse">
       <thead>
         <tr className="border-b">
-          {
-            productColumns.map(p=>(
-              <th key={p.key} className="p-3 text-left">{p.label}</th>
-            ))
-          }
+          {productColumns.map((p) => (
+            <th key={p.key} className="p-3 text-left">
+              {p.label}
+            </th>
+          ))}
         </tr>
       </thead>
 
       <tbody>
         {products.map((product) => (
-          <tr
-            key={product.id}
-            className="border-b hover:bg-gray-50"
-          >
+          <tr key={product.id} className="border-b hover:bg-gray-50">
             <td className="p-3">
               <Image
                 src={product.thumbnail}
@@ -45,9 +43,20 @@ export default function DataTable({
 
             <td className="p-3">{product.stock}</td>
 
-            <td className="p-3">
-              {product.category}
-            </td>
+            <td className="p-3">{product.category}</td>
+<td className="space-x-2">
+  <Button size="icon" variant="outline">
+    <Eye className="h-4 w-4" />
+  </Button>
+
+  <Button size="icon" variant="outline">
+    <Pencil className="h-4 w-4" />
+  </Button>
+
+  <Button size="icon" variant="destructive">
+    <Trash2 className="h-4 w-4" />
+  </Button>
+</td>
           </tr>
         ))}
       </tbody>
